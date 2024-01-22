e9d9245f5acb5f81b8dd74f00af9f4e70c340954
nyma jalalabadi <nima.jalalabadii@gmail.com>
2024-01-22 19:52:22 +0330

    Complete UserSideBar

---
+++ BugFixer.Web/Areas/UserPanel/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using BugFixer.domain.ViewModels.UserPanel.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugFixer.Web.Areas.UserPanel.Controllers
+{
+    public class AccountController : UserPanelBaseController
+    {
+        #region constractor
+
+
+
+        #endregion
+
+        #region edit user info
+
+        [HttpGet]
+        public async Task<IActionResult> EditInfo()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditInfo(EditUserViewModel edit)
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
