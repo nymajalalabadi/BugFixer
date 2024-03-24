99c8bedf7978a22cca713e38c3ad256f3800c603
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-03-24 10:55:18 +0330

    Complete Side Bar

---
+++ BugFixer.Web/ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,99 @@
+﻿using BugFixer.Application.Services.Interfaces;
+using BugFixer.domain.ViewModels.Question;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugFixer.Web.ViewComponents
+{
+    #region Score Des Questions
+
+    public class ScoreDesQuestionsViewComponent : ViewComponent
+    {
+        #region Constractor
+
+        private IQuestionService _questionService;
+
+        public ScoreDesQuestionsViewComponent(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        #endregion
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var options = new FilterQuestionViewModel
+            {
+                TakeEntity = 5,
+                Sort = FilterQuestionSortEnum.ScoreHighToLow
+            };
+
+            var result = await _questionService.FilterQuestion(options);
+
+            return View("ScoreDesQuestions", options);
+        }
+    }
+
+    #endregion
+
+    #region Create Date Des Questions
+
+    public class CreateDateDesQuestionsViewComponent : ViewComponent
+    {
+        #region Constractor
+
+        private IQuestionService _questionService;
+
+        public CreateDateDesQuestionsViewComponent(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        #endregion
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var options = new FilterQuestionViewModel
+            {
+                TakeEntity = 5,
+                Sort = FilterQuestionSortEnum.NewToOld
+            };
+
+            var result = await _questionService.FilterQuestion(options);
+
+            return View("CreateDateDesQuestions", options);
+        }
+    }
+
+    #endregion
+
+    #region Use Count Des Tags
+
+    public class UseCountDesTagsViewComponent : ViewComponent
+    {
+        #region Constractor
+
+        private IQuestionService _questionService;
+
+        public UseCountDesTagsViewComponent(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        #endregion
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var options = new FilterTagViewModel
+            {
+                TakeEntity = 10,
+                Sort = FilterTagEnum.UseCountHighToLow
+            };
+
+            var result = await _questionService.FilterTags(options);
+
+            return View("UseCountDesTags", result);
+        }
+    }
+
+    #endregion
+}
