d6d1127aa21c46fdc85585ae97b1d377bc617598
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-04-18 21:50:12 +0330

    Add New Question Controller For Admin And Add Questions List View

---
+++ BugFixer.Web/Areas/Admin/Controllers/QuestionController.cs
@@ -0,0 +1,28 @@
+﻿using BugFixer.Application.Services.Interfaces;
+using BugFixer.domain.ViewModels.Question;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugFixer.Web.Areas.Admin.Controllers
+{
+    public class QuestionController : AdminBaseController
+    {
+        #region Consractor
+
+        private readonly IQuestionService _questionService;
+
+        public QuestionController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        #endregion
+
+        public async Task<IActionResult> QuestionsList(FilterQuestionViewModel filter)
+        {
+            var result = await _questionService.FilterQuestion(filter);
+
+            return View(result);
+        }
+
+    }
+}
