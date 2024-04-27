43e71700851bf2155b4ee706ff62ada3d9dcd019
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-04-27 10:53:38 +0330

    Add Change Avatar In User Controller

--- BugFixer.Web/Areas/Admin/Controllers/UserController.cs
+++ BugFixer.Web/Areas/Admin/Controllers/UserController.cs
@@ -1,16 +1,19 @@
-﻿using BugFixer.Application.Services.Interfaces;
+﻿using BugFixer.Application.Extensions;
+using BugFixer.Application.Services.Interfaces;
+using BugFixer.Application.Statics;
 using BugFixer.domain.ViewModels.Admin.User;
+using BugFixer.Web.Extentions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BugFixer.Web.Areas.Admin.Controllers
 {
-    public class UserController : Controller
+    public class UserController : AdminBaseController
     {
         #region constractor
 
         private readonly IUserService _userService;
 
-        public UserController(IUserService userService) 
+        public UserController(IUserService userService)
         {
             _userService = userService;
         }
@@ -43,5 +46,28 @@ namespace BugFixer.Web.Areas.Admin.Controllers
         }
 
         #endregion
+
+        #region Change User Avatar
+
+        public async Task<IActionResult> ChangeUserAvatar(IFormFile userAvatar, long userId)
+        {
+            var fileName = Guid.NewGuid().ToString("N") + Path.GetExtension(userAvatar.FileName);
+
+            var result = userAvatar.AddImageToServer(fileName, PathTools.UserAvatarServerPath, 150, 150, PathTools.UserAvatarServerThumb);
+
+            if (!result)
+            {
+                return JsonResponseStatus.Error();
+            }
+
+            await _userService.ChangeAvatarForUser(userId, fileName);
+
+            TempData[SuccessMessage] = "عملیات با موفقیت انجام شد .";
+
+            return JsonResponseStatus.Success();
+        }
+
+        #endregion
+
     }
 }
